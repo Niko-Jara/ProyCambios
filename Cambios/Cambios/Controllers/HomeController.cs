bafed6bce624ca7d57f8cb2d115f289df293f80f
Niko-Jara <jaramnikol@gmail.com>
2020-07-09 22:55:15 -0600

    Inicio Proyecto

---
+++ Cambios/Cambios/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cambios.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //PANTALLAS PORTAL//
+        public ActionResult Pantusuario()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Pantadmin()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Panttec()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        //PANTALLAS GESTION CAMBIOS//
+
+        public ActionResult Cambios()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Regusers()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Aprobaciones()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
---
+++ Cambios/Cambios/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cambios.MvcApplication" Language="C#" %>
